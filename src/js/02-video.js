beb9965cf3f336b1934e27729f7891d0adad414a
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-18 23:50:20 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+let currentTimeSaved = localStorage.getItem('videoplayer-current-time');
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', function (data) {
+  let currentTime = data.seconds;
+  console.log(`Текущее время видео:`, currentTime);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+});
+
+player
+  .setCurrentTime(currentTimeSaved)
+  .then(function (seconds) {
+    console.log(`Время остановки видео:`, seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
