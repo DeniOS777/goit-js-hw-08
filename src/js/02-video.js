0d4a431ad5f2b5bc6289cfe5833eb94d81fe5b57
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-19 15:35:24 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,28 +2,27 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-
 const player = new Player(iframe);
 
 let currentTimeSaved = JSON.parse(localStorage.getItem('videoplayer-current-time'));
 
 player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
+  console.log('Title:', title);
 });
 
 player.on(
   'timeupdate',
   throttle(function (data) {
     let currentTime = data.seconds.toFixed();
-    console.log(`Текущее время видео:`, currentTime);
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+    console.log(`Текущее время видео в секундах:`, currentTime);
+    localStorage.setItem('videoplayer-current-time', currentTime);
   }, 1000),
 );
 
 player
   .setCurrentTime(currentTimeSaved)
   .then(function (seconds) {
-    console.log(`Время остановки видео:`, seconds);
+    console.log(`Время остановки видео на секунде:`, seconds);
   })
   .catch(function (error) {
     switch (error.name) {
