7c65a13cc04ac9187540b5b0206b944905699015
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-19 23:47:14 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+let saveDataForm;
+
+onLoad();
+
+function onLoad() {
+  saveDataForm = localStorage.getItem('feedback-form-state');
+  const dataLS = JSON.parse(saveDataForm);
+  if (!saveDataForm) {
+    return;
+  }
+  refs.input.value = dataLS.email;
+  refs.textarea.value = dataLS.message;
+}
+
+function onInput(evt) {
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  const obj = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+}
+
+function onButtonClickClearForm(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(saveDataForm));
+  localStorage.removeItem('feedback-form-state');
+  refs.form.reset();
+}
+
+refs.form.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', onButtonClickClearForm);
