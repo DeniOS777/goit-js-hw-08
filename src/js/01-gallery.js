abb8d475df8c22c02eaf269cec9f095b170fc3e9
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-30 21:40:00 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,6 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
 
 const listGalleryRef = document.querySelector('.gallery');
 
